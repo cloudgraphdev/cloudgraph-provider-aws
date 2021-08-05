53bcba0f7f924c917778a5c1ec73ad9137ba93d5
Christopher Brandt <christopher.brandt@autocloud.dev>
2021-08-05 15:56:07 -0700

    feat(terraform): add terraform/localstack integration, add README, update tests

--- tests/aws_cloudwatch.test.ts
+++ tests/aws_cloudwatch.test.ts
@@ -1,8 +1,5 @@
 // file: cloudwatch.test.ts
-import { CloudWatch } from 'aws-sdk'
 import CloudGraph from '@cloudgraph/sdk'
-
-import environment from '../src/config/environment'
 import Cloudwatch from '../src/services/cloudwatch'
 
 // TODO: Probably solved by ENG-89
@@ -13,48 +10,10 @@ const credentials = {
 
 // TODO: Single region for now to match free license Localstack limitation
 const regions = 'us-east-1'
-const cloudwatch = new CloudWatch({
-  region: regions,
-  credentials,
-  endpoint: environment.LOCALSTACK_AWS_ENDPOINT,
-})
-
-// TODO: Will be better implemented using a terraform integration
-const testId = 'Instance i-1234567890abcdef0 CPU Utilization'
-beforeAll(done => {
-  cloudwatch.putMetricAlarm(
-    {
-      Namespace: 'AWS/EC2',
-      MetricName: 'CPUUtilization',
-      Dimensions: [
-        {
-          Name: 'InstanceId',
-          Value: 'i-1234567890abcdef0',
-        },
-      ],
-      AlarmActions: ['arn:aws:sns:us-west-1:123456789012:my_sns_topic'],
-      ComparisonOperator: 'GreaterThanThreshold',
-      DatapointsToAlarm: 3,
-      EvaluationPeriods: 4,
-      Period: 60,
-      Statistic: 'Average',
-      Threshold: 40,
-      AlarmDescription:
-        'CPU Utilization of i-1234567890abcdef0 with 40% as threshold',
-      AlarmName: 'Instance i-1234567890abcdef0 CPU Utilization',
-      Tags: [{ Key: 'testTag', Value: 'TestValue' }],
-    },
-    () => done()
-  )
-})
 
 test('should be a valid request', async () => {
   const config = { logger: CloudGraph.logger }
   const classInstance = new Cloudwatch(config)
   const response = await classInstance.getData({ credentials, regions })
   expect(response[regions][0].tags).toBeDefined()
-})
-
-afterAll(done => {
-  cloudwatch.deleteAlarms({ AlarmNames: [testId] }, () => done())
 })
