b41f16bd57d93f7bcaf62b187ccedbe67781744e
Marco Franceschi <marco.franceschi@autocloud.dev>
2021-08-05 14:11:13 -0400

    feat(ec2): Adding types to getData method

--- src/services/ec2/data.ts
+++ src/services/ec2/data.ts
@@ -3,17 +3,25 @@ import * as Sentry from '@sentry/node'
 import groupBy from 'lodash/groupBy'
 import isEmpty from 'lodash/isEmpty'
 
-import EC2 from 'aws-sdk/clients/ec2'
-import { Opts } from '@cloudgraph/sdk'
+import EC2, {
+  DescribeInstancesRequest,
+  DescribeInstancesResult,
+  DescribeKeyPairsResult,
+  DescribeTagsRequest,
+  DescribeTagsResult,
+  Instance,
+  InstanceAttribute,
+  TagList,
+} from 'aws-sdk/clients/ec2'
+import { AWSError } from 'aws-sdk/lib/error'
+
+import CloudGraph from '@cloudgraph/sdk'
 
 import { Credentials } from '../../types'
+import awsLoggerText from '../../properties/logger'
 
-// import { logger } from '../../../../../middleware'
-
-// import awsLoggerText from '../../properties/logger'
-// import { commonVsdLoggerText } from '../../../../shared/visualServiceDiscovery/properties/logger'
-
-// const lt = { ...commonVsdLoggerText, ...awsLoggerText }
+const lt = { ...awsLoggerText }
+const { logger } = CloudGraph
 
 /**
  * EC2
@@ -22,18 +30,24 @@ import { Credentials } from '../../types'
 export default async ({
   regions,
   credentials,
-}: // opts,
-{
+}: {
   regions: string
   credentials: Credentials
-  opts: Opts
-}) =>
+}): Promise<{
+  [region: string]: (Instance & {
+    region: string
+    DisableApiTermination?: boolean
+    KeyPairName?: string
+    Tags?: TagList
+  })[]
+}> =>
   new Promise(async resolve => {
-    const tagsPromises = []
-    const ec2Instances = []
-    const regionPromises = []
-    const keyPairPromises = []
-    const deletionProtectionPromises = []
+    const ec2Instances: (Instance & {
+      region: string
+      DisableApiTermination?: boolean
+      KeyPairName?: string
+      Tags?: TagList
+    })[] = []
 
     /**
      * Step 1) for all regions, list the EC2 Instances
@@ -44,82 +58,79 @@ export default async ({
       region,
       nextToken: NextToken = '',
       resolveRegion,
-    }) => {
-      let args: any = {}
+    }: {
+      ec2: EC2
+      region: string
+      nextToken?: string
+      resolveRegion: () => void
+    }): Promise<any> => {
+      let args: DescribeInstancesRequest = {}
 
       if (NextToken) {
         args = { ...args, NextToken }
       }
 
-      return ec2.describeInstances(args, (err, data) => {
-        if (err) {
-          // logger.error(err, err.stack)
-          Sentry.captureException(new Error(err.message))
+      return ec2.describeInstances(
+        args,
+        (err: AWSError, data: DescribeInstancesResult) => {
+          if (err) {
+            logger.error(err)
+            Sentry.captureException(new Error(err.message))
+          }
+
+          /**
+           * No EC2 data for this region
+           */
+          if (isEmpty(data)) {
+            return resolveRegion()
+          }
+
+          const { NextToken: nextToken, Reservations: res = [] } = data || {}
+
+          const instances = res
+            .map(({ Instances }) => Instances)
+            .reduce((current, acc) => [...acc, ...current], [])
+
+          logger.debug(lt.fetchedEc2Instances(instances.length))
+
+          /**
+           * No EC2 Instances Found
+           */
+
+          if (isEmpty(instances)) {
+            return resolveRegion()
+          }
+
+          /**
+           * Check to see if there are more
+           */
+
+          if (nextToken) {
+            listEc2Instances({ region, nextToken, ec2, resolveRegion })
+          }
+
+          ec2Instances.push(
+            ...instances.map(instance => ({ ...instance, region }))
+          )
+
+          /**
+           * If this is the last page of data then return the instances
+           */
+
+          if (!nextToken) {
+            resolveRegion()
+          }
         }
-
-        /**
-         * No EC2 data for this region
-         */
-        if (isEmpty(data)) {
-          return resolveRegion()
-        }
-
-        const { NextToken: nextToken, Reservations: res = [] } = data || {}
-
-        const instances = res.flatMap(({ Instances }) => Instances)
-
-        // logger.info(lt.fetchedEc2Instances(instances.length))
-
-        /**
-         * No EC2 Instances Found
-         */
-
-        if (isEmpty(instances)) {
-          return resolveRegion()
-        }
-
-        /**
-         * Check to see if there are more
-         */
-
-        if (nextToken) {
-          listEc2Instances({ region, nextToken, ec2, resolveRegion })
-        }
-
-        /**
-         * If there are not, then add these to the ec2Instances and convert the tags
-         * To the correct shape [{ Key1: Value1 }] -> { Key1: Value1 }
-         */
-
-        const Tags = {}
-
-        instances.map(({ Tags }) =>
-          Tags.map(({ Key, Value }) => {
-            Tags[Key] = Value
-          })
-        )
-
-        ec2Instances.push(
-          ...instances.map(instance => ({ ...instance, Tags, region }))
-        )
-
-        /**
-         * If this is the last page of data then return the instances
-         */
-
-        if (!nextToken) {
-          resolveRegion()
-        }
-      })
+      )
     }
 
-    regions.split(',').map(region => {
+    const regionPromises = regions.split(',').map(region => {
       const ec2 = new EC2({ region, credentials })
 
       const regionPromise = new Promise<void>(resolveRegion =>
         listEc2Instances({ ec2, region, resolveRegion })
       )
-      regionPromises.push(regionPromise)
+      return regionPromise
     })
 
     await Promise.all(regionPromises)
@@ -128,86 +139,89 @@ export default async ({
      * Step 2) Get the Key Pair names for each instance's key pair
      */
 
-    // let totalKeyPairs = 0
-
-    ec2Instances.map(({ region }, ec2Idx) => {
+    const keyPairPromises = ec2Instances.map(({ region }, ec2Idx) => {
       const ec2 = new EC2({ region, credentials })
 
       const keyPairPromise = new Promise<void>(resolveKeyPair =>
-        ec2.describeKeyPairs({}, (err, data) => {
-          if (err) {
-            // logger.error(err, err.stack)
-            Sentry.captureException(new Error(err.message))
+        ec2.describeKeyPairs(
+          {},
+          (err: AWSError, data: DescribeKeyPairsResult) => {
+            if (err) {
+              logger.error(err)
+              Sentry.captureException(new Error(err.message))
+            }
+
+            /**
+             * No Key Pair data for this instance
+             */
+
+            if (isEmpty(data)) {
+              return resolveKeyPair()
+            }
+
+            const { KeyPairs: pairs } = data || {}
+
+            logger.debug(`${lt.fetchedKeyPairs(pairs.length)} at ${region}`)
+
+            /**
+             * No Key Pairs Found
+             */
+
+            if (isEmpty(pairs)) {
+              return resolveKeyPair()
+            }
+
+            ec2Instances[ec2Idx].KeyPairName = pairs
+              .map(({ KeyName }) => KeyName)
+              .join(', ')
+
+            resolveKeyPair()
           }
-
-          /**
-           * No Key Pair data for this instance
-           */
-
-          if (isEmpty(data)) {
-            return resolveKeyPair()
-          }
-
-          const { KeyPairs: pairs } = data || {}
-
-          /**
-           * No Key Pairs Found
-           */
-
-          if (isEmpty(pairs)) {
-            return resolveKeyPair()
-          }
-
-          ec2Instances[ec2Idx].keyPairName = pairs
-            .map(({ KeyName }) => KeyName)
-            .join(', ')
-          // totalKeyPairs += 1
-          resolveKeyPair()
-        })
+        )
       )
-      keyPairPromises.push(keyPairPromise)
+      return keyPairPromise
     })
 
     await Promise.all(keyPairPromises)
-
-    // logger.info(lt.fetchedKeyPairs(totalKeyPairs))
 
     /**
      * Step 3) check to see if disableApiTermination is on for each instance
      */
 
-    ec2Instances.map(({ region, InstanceId }, ec2Idx) => {
-      const ec2 = new EC2({ region, credentials })
+    const deletionProtectionPromises = ec2Instances.map(
+      ({ region, InstanceId }, ec2Idx) => {
+        const ec2 = new EC2({ region, credentials })
 
-      const deletionProtectionPromise = new Promise<void>(
-        resolveDeletionProtection =>
-          ec2.describeInstanceAttribute(
-            { InstanceId, Attribute: 'disableApiTermination' },
-            (err, data) => {
-              if (err) {
-                // logger.error(err, err.stack)
-                Sentry.captureException(new Error(err.message))
+        const deletionProtectionPromise = new Promise<void>(
+          resolveDeletionProtection =>
+            ec2.describeInstanceAttribute(
+              { InstanceId, Attribute: 'disableApiTermination' },
+              (err: AWSError, data: InstanceAttribute) => {
+                if (err) {
+                  logger.error(err)
+                  Sentry.captureException(new Error(err.message))
+                }
+
+                /**
+                 * No data
+                 */
+                if (isEmpty(data)) {
+                  return resolveDeletionProtection()
+                }
+
+                const {
+                  DisableApiTermination: { Value: val },
+                } = data || { DisableApiTermination: {} }
+
+                ec2Instances[ec2Idx].DisableApiTermination = val
+                resolveDeletionProtection()
               }
+            )
+        )
 
-              /**
-               * No data
-               */
-
-              if (isEmpty(data)) {
-                return resolveDeletionProtection()
-              }
-
-              const {
-                DisableApiTermination: { Value: val },
-              } = data || { DisableApiTermination: {} }
-
-              ec2Instances[ec2Idx].DisableApiTermination = val
-              resolveDeletionProtection()
-            }
-          )
-      )
-      deletionProtectionPromises.push(deletionProtectionPromise)
-    })
+        return deletionProtectionPromise
+      }
+    )
 
     await Promise.all(deletionProtectionPromises)
 
@@ -223,8 +237,13 @@ export default async ({
       region,
       nextToken: NextToken = '',
       resolveTags,
-    }) => {
-      let args: any = {
+    }: {
+      ec2: EC2
+      region: string
+      nextToken?: string
+      resolveTags: () => void
+    }): Promise<any> => {
+      let args: DescribeTagsRequest = {
         Filters: [{ Name: 'resource-type', Values: ['instance'] }],
       }
 
@@ -232,66 +251,69 @@ export default async ({
         args = { ...args, NextToken }
       }
 
-      return ec2.describeTags(args, (err, data) => {
-        if (err) {
-          // logger.error(err, err.stack)
-          Sentry.captureException(new Error(err.message))
-        }
-
-        /**
-         * No tag data for this region
-         */
-        if (isEmpty(data)) {
-          return resolveTags()
-        }
-
-        const { NextToken: nextToken, Tags: tags = [] } = data || {}
-
-        // logger.info(lt.fetchedEc2InstanceTags(tags.length))
-
-        /**
-         * No Tags Found
-         */
-
-        if (isEmpty(tags)) {
-          return resolveTags()
-        }
-
-        /**
-         * Check to see if there are more
-         */
-
-        if (nextToken) {
-          listAllTagsforAllInstances({
-            region,
-            nextToken,
-            ec2,
-            resolveTags,
-          })
-        }
-
-        /**
-         * Add the tags to the allTags Cache
-         */
-
-        tags.map(({ Key, Value, ResourceId }) => {
-          if (!allTags[ResourceId]) {
-            allTags[ResourceId] = {}
+      return ec2.describeTags(
+        args,
+        (err: AWSError, data: DescribeTagsResult) => {
+          if (err) {
+            logger.error(err)
+            Sentry.captureException(new Error(err.message))
           }
-          allTags[ResourceId][Key] = Value
-        })
 
-        /**
-         * If this is the last page of data then return the instances
-         */
+          /**
+           * No tag data for this region
+           */
+          if (isEmpty(data)) {
+            return resolveTags()
+          }
 
-        if (!nextToken) {
-          resolveTags()
+          const { NextToken: nextToken, Tags: tags = [] } = data || {}
+
+          logger.debug(lt.fetchedEc2InstanceTags(tags.length))
+
+          /**
+           * No Tags Found
+           */
+
+          if (isEmpty(tags)) {
+            return resolveTags()
+          }
+
+          /**
+           * Check to see if there are more
+           */
+
+          if (nextToken) {
+            listAllTagsforAllInstances({
+              region,
+              nextToken,
+              ec2,
+              resolveTags,
+            })
+          }
+
+          /**
+           * Add the tags to the allTags Cache
+           */
+
+          tags.map(({ Key, Value, ResourceId }) => {
+            if (!allTags[ResourceId]) {
+              allTags[ResourceId] = {}
+            }
+            allTags[ResourceId] = [...allTags[ResourceId], { Key, Value }]
+          })
+
+          /**
+           * If this is the last page of data then return the instances
+           */
+
+          if (!nextToken) {
+            resolveTags()
+          }
         }
-      })
+      )
     }
 
-    regions.split(',').map(region => {
+    const tagsPromises = regions.split(',').map(region => {
       const ec2 = new EC2({ region, credentials })
       const tagPromise = new Promise<void>(resolveTags =>
         listAllTagsforAllInstances({
@@ -300,7 +322,7 @@ export default async ({
           resolveTags,
         })
       )
-      tagsPromises.push(tagPromise)
+      return tagPromise
     })
 
     await Promise.all(tagsPromises)
