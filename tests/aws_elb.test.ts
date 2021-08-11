2cdfd7812162bb12c48d8044b6911a06dc94f401
Marco Franceschi <marco.franceschi@autocloud.dev>
2021-08-11 15:29:41 -0400

    feat(elb): Added TODO unit tests boilerplate

---
+++ tests/aws_elb.test.ts
@@ -0,0 +1,13 @@
+describe('ELB Service Test: ', () => {
+  it.todo('should return a truthy value ')
+
+  it.todo('getData: should return data from a region in the correct format')
+
+  it.todo(
+    'format: should return data in wthe correct format matching the schema type'
+  )
+
+  it.todo('getConnections(subnet): should create the connection to subnet')
+
+  it.todo('getConnections(sg): should create the connection to security groups')
+})
