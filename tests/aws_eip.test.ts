4fbd109884022695b61ec68a429845fde9bf01dc
Marco Franceschi <marco.franceschi@autocloud.dev>
2021-08-17 17:16:17 -0400

    fix: Completed EIP tests

--- tests/aws_eip.test.ts
+++ tests/aws_eip.test.ts
@@ -1,75 +1,83 @@
-import CloudGraph/* , { ServiceConnection } */from '@cloudgraph/sdk'
+import CloudGraph from '@cloudgraph/sdk'
+import EIPService from '../src/services/eip'
+import { account, credentials, region } from '../src/properties/test'
+import { initTestConfig } from '../src/utils'
 
-// import EC2Class from '../src/services/ec2'
-import Eip from '../src/services/eip'
-import { credentials, region } from '../src/properties/test'
-// import Vpc from '../src/services/vpc'
-// import services from '../src/enums/services'
-
-jest.setTimeout(30000)
-
-let eipGetDataResponse
-// let initiatorTestData
-// let initiatorGetConnectionsResult
-beforeAll(async () => {
-  try {
-    // const ec2Class = new EC2Class({ logger: CloudGraph.logger })
-    // const ec2GetDataResponse = await ec2Class.getData({
-    //   credentials,
-    //   regions: region,
-    // })
-    const eipClass = new Eip({ logger: CloudGraph.logger })
-    // const vpcClass = new Vpc({ logger: CloudGraph.logger })
-    eipGetDataResponse = await eipClass.getData({
-      credentials,
-      regions: region,
-    })
-    // initiatorTestData = [
-    //   {
-    //     name: services.vpc,
-    //     data: await vpcClass.getData({
-    //       credentials,
-    //       regions: region,
-    //     }),
-    //   },
-    //   {
-    //     name: services.eip,
-    //     data: eipGetDataResponse,
-    //   },
-    //   {
-    //     name: services.ec2Instance,
-    //     data: ec2GetDataResponse,
-    //   },
-    // ]
-    // initiatorGetConnectionsResult = initiatorTestData[0].data[region].map(
-    //   vpc =>
-    //     vpcClass.getConnections({
-    //       service: vpc,
-    //       data: initiatorTestData,
-    //       account,
-    //       region,
-    //     })
-    // )
-  } catch (error) {
-    console.error(error) // eslint-disable-line no-console
-  }
-  return Promise.resolve()
-})
+let eipGetDataResult
+let formatResult
 
 describe('EIP Service Test: ', () => {
-  test('Should return an array of addresses grouped by region', async () => {
-    expect(Object.keys(eipGetDataResponse)[0]).toBe(region)
-    expect(eipGetDataResponse[region].length).toBeGreaterThan(0)
+  initTestConfig()
+  beforeAll(
+    async () =>
+      new Promise<void>(async resolve => {
+        eipGetDataResult = {}
+        formatResult = {}
+        try {
+          const eipService = new EIPService({ logger: CloudGraph.logger })
+
+          // Get EIP data
+          eipGetDataResult = await eipService.getData({
+            credentials,
+            regions: region,
+          })
+
+          // Format EIP data
+          formatResult = eipGetDataResult[region].map(eipData =>
+            eipService.format({ service: eipData, region, account })
+          )
+        } catch (error) {
+          console.error(error) // eslint-disable-line no-console
+        }
+        resolve()
+      })
+  )
+
+  it('should return a truthy value ', () => {
+    expect(eipGetDataResult).toBeTruthy()
   })
 
-  test('initiator(vpc) should create the connection to eip', () => {
-    // const vpcEipConnections: ServiceConnection[] =
-    //   initiatorGetConnectionsResult
-    //     ?.find(v => vpcData.Vpc.VpcId in v)
-    //     [vpcData.Vpc.VpcId].find(
-    //       (c: ServiceConnection) =>
-    //         c.resourceType === services.eip && c.id === eipData.AllocationId
-    //     ) || undefined
-    // expect(vpcEipConnections).toBeTruthy()
+  it('getData: should return data from a region in the correct format', () => {
+    expect(eipGetDataResult[region]).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          AllocationId: expect.any(String),
+          AssociationId: expect.any(String),
+          Domain: expect.any(String),
+          InstanceId: expect.any(String),
+          NetworkInterfaceId: expect.any(String),
+          PublicIp: expect.any(String),
+          region: expect.any(String),
+        }),
+      ])
+    )
   })
+
+  it('format: should return data in the correct format matching the schema type', () => {
+    expect(formatResult).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(String),
+          arn: expect.any(String),
+          vpc: expect.any(String),
+          domain: expect.any(String),
+          ec2InstanceAssociationId: expect.any(String),
+          instanceId: expect.any(String),
+          publicIp: expect.any(String),
+          // Optional fields
+          // customerOwnedIp: expect.any(String),
+          // customerOwnedIpv4Pool: expect.any(String),
+          // networkBorderGroup: expect.any(String),
+          // networkInterface: expect.any(String),
+          // networkInterfaceOwnerId: expect.any(String),
+          // publicIpv4Pool: expect.any(String),
+          // privateIp: expect.any(String),
+        }),
+      ])
+    )
+  })
+
+  it.todo(
+    'getConnections(networkInterface): should verify the connection to networkInterface'
+  )
 })
