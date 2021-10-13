cb5ae526df44725b3722bd9dab7ef284281996c1
Christopher Brandt <christopher.brandt@autocloud.dev>
2021-10-13 07:49:16 -0700

    fix(secretsManager): update SecretsManager class name

--- src/services/secretsManager/index.ts
+++ src/services/secretsManager/index.ts
@@ -4,7 +4,7 @@ import format from './format'
 import getData from './data'
 import mutation from './mutation'
 
-export default class S3 extends BaseService implements Service {
+export default class SecretsManager extends BaseService implements Service {
   format = format.bind(this)
 
   getData = getData.bind(this)
