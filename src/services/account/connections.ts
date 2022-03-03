eb44044346ead49596f5bf22394c2a442e70ee92
Marco Franceschi <marco.franceschi@autocloud.dev>
2022-03-03 16:58:12 -0300

    feat: Created account global service

---
+++ src/services/account/connections.ts
@@ -0,0 +1,49 @@
+import { ServiceConnection } from '@cloudgraph/sdk'
+import { flatMap } from 'lodash'
+import services from '../../enums/services'
+
+export default ({
+  service,
+  data,
+}: {
+  service: any
+  data: Array<{
+    name: string
+    accountId: string
+    data: { [property: string]: any[] }
+  }>
+}): {
+  [property: string]: ServiceConnection[]
+} => {
+  const { id } = service
+  const connections: ServiceConnection[] = []
+  const connectTo = Object.values(services).filter(s => s !== 'account')
+
+  for (const serviceName of connectTo) {
+    const instances: {
+      name: string
+      data: { [property: string]: any[] }
+    } = data.find(({ name }) => {
+      return name === serviceName
+    })
+
+    if (instances?.data) {
+      const filtered = flatMap(instances.data).filter(i => i.accountId === id)
+
+      for (const instance of filtered) {
+        if (instance) {
+          connections.push({
+            id: instance.id,
+            resourceType: serviceName,
+            relation: 'child',
+            field: serviceName,
+          })
+        }
+      }
+    }
+  }
+
+  return {
+    [id]: connections,
+  }
+}
