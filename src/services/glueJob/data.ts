e33ff2ed1762696e58b19015ff2b841888fc9ce4
Marco Franceschi <marco.franceschi@autocloud.dev>
2022-03-04 19:07:25 -0300

    fix: Fixed glueJob and glueRegistry issues during scan

--- src/services/glueJob/data.ts
+++ src/services/glueJob/data.ts
@@ -1,6 +1,6 @@
 import { Config } from 'aws-sdk/lib/config'
 import GLUE from 'aws-sdk/clients/glue'
-import { groupBy } from 'lodash'
+import { groupBy, isEmpty } from 'lodash'
 import { convertToPromise, fetchAllPaginatedData } from '../../utils/fetchUtils'
 import { initTestEndpoint } from '../../utils'
 import ErrorLog from '../../utils/errorLog'
@@ -23,29 +23,31 @@ export default async ({
 }: {
   regions: string
   config: Config
-}): Promise<{ [region: string]: RawAwsGlueJob[]}> => {
+}): Promise<{ [region: string]: RawAwsGlueJob[] }> => {
   const result: RawAwsGlueJob[] = []
 
   const activeRegions = regions.split(',')
 
   for (const region of activeRegions) {
-    let glueJobData: GLUE.Job[]
+    let glueJobData: GLUE.Job[] = []
     try {
-      glueJobData = await fetchAllPaginatedData({
-        getResourcesFn: convertToPromise({
-          sdkContext: new GLUE({ ...config, region, endpoint }),
-          fnName: 'getJobs',
-        }),
-        accessor: '',
-      }) ?? []
+      glueJobData =
+        (await fetchAllPaginatedData({
+          getResourcesFn: convertToPromise({
+            sdkContext: new GLUE({ ...config, region, endpoint }),
+            fnName: 'getJobs',
+          }),
+          accessor: '',
+        })) ?? []
     } catch (err) {
       errorLog.generateAwsErrorLog({
         functionName: 'glueJob:getJobs',
-        err
+        err,
       })
     }
 
-    result.push(...glueJobData.map(val => ({ ...val, region })))
+    if (!isEmpty(glueJobData))
+      result.push(...glueJobData.map(val => ({ ...val, region })))
   }
   errorLog.reset()
   return groupBy(result, 'region')
