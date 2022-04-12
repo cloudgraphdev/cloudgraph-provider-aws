6480e7f16c404ebb2c7f2f458aa95ae2db93f0dc
Christopher Brandt <christopher.brandt@autocloud.dev>
2022-04-12 16:43:24 -0700

    feat(rdsCluster): add route53HostedZone connection

--- src/services/rdsCluster/connections.ts
+++ src/services/rdsCluster/connections.ts
@@ -1,10 +1,11 @@
 import { ServiceConnection } from '@cloudgraph/sdk'
 import { isEmpty } from 'lodash'
 import { SecurityGroup } from 'aws-sdk/clients/ec2'
-import { DBInstance, DBCluster } from 'aws-sdk/clients/rds'
+import { DBInstance } from 'aws-sdk/clients/rds'
 
 import services from '../../enums/services'
 import { RawAwsRdsCluster } from './data'
+import { RawAwsRoute53HostedZone } from '../route53HostedZone/data'
 import { RawAwsRdsClusterSnapshot } from '../rdsClusterSnapshot/data'
 import { RawAwsIamRole } from '../iamRole/data'
 import { RawAwsSubnet } from '../subnet/data'
@@ -33,6 +34,7 @@ export default ({
     ActivityStreamKmsKeyId,
     PerformanceInsightsKMSKeyId,
     VpcSecurityGroups,
+    HostedZoneId: hostedZoneId,
   } = service
   const sgIds = VpcSecurityGroups.map(
     ({ VpcSecurityGroupId }) => VpcSecurityGroupId
@@ -125,9 +127,11 @@ export default ({
   } = data.find(({ name }) => name === services.kms)
 
   if (kms?.data?.[region]) {
-    // set storage encryption kms key
-    let kmsInRegion: AwsKms[] = kms.data[region].filter(
-      ({ Arn }: AwsKms) => Arn === KmsKeyId
+    const kmsInRegion: AwsKms[] = kms.data[region].filter(
+      ({ Arn }: AwsKms) =>
+        Arn === KmsKeyId ||
+        Arn === ActivityStreamKmsKeyId ||
+        Arn === PerformanceInsightsKMSKeyId
     )
     if (!isEmpty(kmsInRegion)) {
       for (const instance of kmsInRegion) {
@@ -135,37 +139,7 @@ export default ({
           id: instance.KeyId,
           resourceType: services.kms,
           relation: 'child',
-          field: 'storageEncryptedKms',
-        })
-      }
-    }
-
-    // set activity stream kms key
-    kmsInRegion = kms.data[region].filter(
-      ({ Arn }: AwsKms) => Arn === ActivityStreamKmsKeyId
-    )
-    if (!isEmpty(kmsInRegion)) {
-      for (const instance of kmsInRegion) {
-        connections.push({
-          id: instance.KeyId,
-          resourceType: services.kms,
-          relation: 'child',
-          field: 'activityStreamKms',
-        })
-      }
-    }
-
-    // set performance insights kms key
-    kmsInRegion = kms.data[region].filter(
-      ({ Arn }: AwsKms) => Arn === PerformanceInsightsKMSKeyId
-    )
-    if (!isEmpty(kmsInRegion)) {
-      for (const instance of kmsInRegion) {
-        connections.push({
-          id: instance.KeyId,
-          resourceType: services.kms,
-          relation: 'child',
-          field: 'performanceInsightsKms',
+          field: 'kms',
         })
       }
     }
@@ -181,10 +155,12 @@ export default ({
   } = data.find(({ name }) => name === services.iamRole)
 
   if (iamRoles?.data?.[globalRegionName]) {
-    let iamRolesInRegion: RawAwsIamRole[] = iamRoles.data[
+    const iamRolesInRegion: RawAwsIamRole[] = iamRoles.data[
       globalRegionName
-    ].filter(({ Arn }: RawAwsIamRole) =>
-      associatedRoles.find(r => r.RoleArn === Arn)
+    ].filter(
+      ({ Arn }: RawAwsIamRole) =>
+        Arn === monitoringRoleArn ||
+        associatedRoles.find(r => r.RoleArn === Arn)
     )
     if (!isEmpty(iamRolesInRegion)) {
       for (const instance of iamRolesInRegion) {
@@ -196,36 +172,29 @@ export default ({
         })
       }
     }
-    iamRolesInRegion = iamRoles.data[globalRegionName].filter(
-      ({ Arn }: RawAwsIamRole) => Arn === monitoringRoleArn
-    )
-    if (!isEmpty(iamRolesInRegion)) {
-      for (const instance of iamRolesInRegion) {
-        connections.push({
-          id: instance.Arn,
-          resourceType: services.iamRole,
-          relation: 'child',
-          field: 'monitoringIamRole',
-        })
-      }
-    }
   }
 
   /**
-   * Find KMS
+   * Find Route53 hosted zones
    */
-  const kmsKeys = data.find(({ name }) => name === services.kms)
-  if (kmsKeys?.data?.[region]) {
-    const kmsKeyInRegion = kmsKeys.data[region].filter(
-      kmsKey => kmsKey.Arn === KmsKeyId
-    )
-    if (!isEmpty(kmsKeyInRegion)) {
-      for (const kms of kmsKeyInRegion) {
+  const route53HostedZones: {
+    name: string
+    data: { [property: string]: RawAwsRoute53HostedZone[] }
+  } = data.find(({ name }) => name === services.route53HostedZone)
+
+  if (route53HostedZones?.data?.[region]) {
+    const instancesInRegion: RawAwsRoute53HostedZone[] =
+      route53HostedZones.data[region].filter(
+        ({ Id }: RawAwsRoute53HostedZone) => Id === hostedZoneId
+      )
+    if (!isEmpty(instancesInRegion)) {
+      for (const instance of instancesInRegion) {
+        const { Id: id } = instance
         connections.push({
-          id: kms.KeyId,
-          resourceType: services.kms,
+          id,
+          resourceType: services.route53HostedZone,
           relation: 'child',
-          field: 'kms',
+          field: 'route53HostedZone',
         })
       }
     }
--- src/types/generated.ts
+++ src/types/generated.ts
@@ -3540,6 +3540,7 @@ export type AwsRdsCluster = AwsBaseService & {
   readerEndpoint?: Maybe<Scalars['String']>;
   replicationSourceIdentifier?: Maybe<Scalars['String']>;
   resourceId?: Maybe<Scalars['String']>;
+  route53HostedZone?: Maybe<Array<Maybe<AwsRoute53HostedZone>>>;
   securityGroups?: Maybe<Array<Maybe<AwsSecurityGroup>>>;
   snapshots?: Maybe<Array<Maybe<AwsRdsClusterSnapshot>>>;
   status?: Maybe<Scalars['String']>;
@@ -3686,6 +3687,7 @@ export type AwsRoute53HostedZone = AwsBaseService & {
   delegationSetId?: Maybe<Scalars['String']>;
   name?: Maybe<Scalars['String']>;
   nameServers?: Maybe<Array<Maybe<Scalars['String']>>>;
+  rdsCluster?: Maybe<Array<Maybe<AwsRdsCluster>>>;
   route53Record?: Maybe<Array<Maybe<AwsRoute53Record>>>;
   vpc?: Maybe<Array<Maybe<AwsVpc>>>;
 };
